352f3789fbeb0409d9ac06bacb5b7cb8f2337620
alhassan50 <abdullahialhassanbaaba@gmail.com>
2024-06-20 13:08:00 +0000

    Add title bar

---
+++ app/components/shared/TitleBar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function TitleBar() {
+  return (
+    <div>
+        <h3 className='font-medium'>
+            My Personal Tutor
+        </h3>
+    </div>
+  )
+}
+
+export default TitleBar
