b442bd7af8781496ee58d408a7d53dddf97e5125
alhassan50 <abdullahialhassanbaaba@gmail.com>
2024-06-21 09:09:49 +0000

    Display title of active nav link on title bar

--- app/components/shared/TitleBar.tsx
+++ app/components/shared/TitleBar.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
+'use client'
+
+import { usePathname } from 'next/navigation'
+import { navLinks } from '@/app/data/navLinks'
 
 function TitleBar() {
+  const pathname = usePathname()
+  const activeNavLink = navLinks.find(link => link.href === pathname)
+
   return (
     <div>
-        <h3 className='font-medium'>
-            My Personal Tutor
+        <h3 className='font-medium capitalize'>
+          {activeNavLink ? activeNavLink.title : 'Not Found'}
         </h3>
     </div>
   )
