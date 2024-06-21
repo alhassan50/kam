4f751cda3f0beee7c0a9eaf2f03a35a15408a637
alhassan50 <abdullahialhassanbaaba@gmail.com>
2024-06-21 12:05:50 +0000

    Fix typo

--- app/components/shared/Profile.tsx
+++ app/components/shared/Profile.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Profile() {
   return (
     <figure className='w-9 h-9 p-2 rounded-[50%] bg-tertiary'>            
-        <svg viewBox="0 0 256 256" xmlns="http://www.w3.org/2000/svg"><rect fill="none" className='w-full h-full'/><circle cx="128" cy="96" fill="none" r="64" stroke="#000" stroke-miterlimit="10" stroke-width="16"/><path d="M31,216a112,112,0,0,1,194,0" fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"/></svg>
+        <svg viewBox="0 0 256 256" xmlns="http://www.w3.org/2000/svg"><rect fill="none" className='w-full h-full'/><circle cx="128" cy="96" fill="none" r="64" stroke="#000" strokeMiterlimit="10" strokeWidth="16"/><path d="M31,216a112,112,0,0,1,194,0" fill="none" stroke="#000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16"/></svg>
     </figure>
   )
 }
