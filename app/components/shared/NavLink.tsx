bbfdbdcbbbd4faeb98e9a48097b72db08847ac0c
alhassan50 <abdullahialhassanbaaba@gmail.com>
2024-06-21 12:07:02 +0000

    Implement sidebar expansion and collapse

--- app/components/shared/NavLink.tsx
+++ app/components/shared/NavLink.tsx
@@ -3,6 +3,8 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import { usePathname } from 'next/navigation';
+import { useSelector } from 'react-redux';
+import { selectisSideBarOpened } from '@/app/redux/slices/sideBarSlice';
 
 type Link = {
     title: string;
@@ -12,12 +14,13 @@ type Link = {
 
 function NavLink({link}: {link: Link}) {
     const pathname = usePathname()
+    const isSideBarOpened = useSelector(selectisSideBarOpened)
 
   return (
     <div className={`capitalize group transition-all duration-200 hover:bg-tertiary ${link.href === '/go-premium' && 'border-t border-b'}`}>
         <Link 
             href={link.href} 
-            className={`py-3 px-6 min-w-full flex items-center gap-2 ${link.href === pathname ? 'active-link' : null}`}
+            className={`py-3 px-6 min-w-full flex flex-nowrap items-center gap-2 ${link.href === pathname ? 'active-link' : null}`}
         >
             <figure className='w-7 h-7 group-hover:scale-125 transition-all duration-200 rounded-[4px]'>
                 <Image 
@@ -29,7 +32,7 @@ function NavLink({link}: {link: Link}) {
                 />
             </figure>
             
-            <h3 className='gro group-hover:font-medium'>
+            <h3 className={`group-hover:font-medium whitespace-nowrap ${isSideBarOpened ? 'opacity-100' : 'opacity-0'}`}>
                 {link.title}
             </h3>
         </Link>
--- app/components/shared/SideBar.tsx
+++ app/components/shared/SideBar.tsx
@@ -1,20 +1,27 @@
+'use client'
+
 import { navLinks } from '@/app/data/navLinks';
 import NavLink from './NavLink';
 import ThemeColor from './ThemeColor';
+import { selectisSideBarOpened } from '@/app/redux/slices/sideBarSlice';
+import { useSelector } from 'react-redux';
 
 function SideBar() {
+    const isSideBarOpened = useSelector(selectisSideBarOpened)
   return (
-    <div className='w-[250px] h-[calc(100vh-53px)] bg-[#fafafa] border-r pt-2'>
-        <ul className=''>
-            {navLinks.map(link => (
-                <li 
-                    key={link.href}
-                >
-                    <NavLink link={link} />
-                </li>
-            ))}
-        </ul>        
-        <ThemeColor />
+    <div className={`${isSideBarOpened ? 'w-[250px]' : 'w-[85px]'} overflow-hidden h-[calc(100vh-53px)] bg-[#fafafa] border-r pt-2 transition-all duration-200`}>
+        <div className={`w-[250px] h-full`}>
+            <ul className=''>
+                {navLinks.map(link => (
+                    <li 
+                        key={link.href}
+                    >
+                        <NavLink link={link} />
+                    </li>
+                ))}
+            </ul>        
+            <ThemeColor />
+        </div>
     </div>
   )
 }
