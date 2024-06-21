3714f9855179f72c9643edda8fa6ccf3fdd3208a
alhassan50 <abdullahialhassanbaaba@gmail.com>
2024-06-21 12:05:06 +0000

    Add redux provider to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Poppins } from "next/font/google";
 import "./globals.css";
 import Header from "./components/shared/Header";
 import SideBar from "./components/shared/SideBar";
+import ReduxProvider from "./redux/ReduxProvider/ReduxProvider";
 
 const poppins = Poppins({ 
   subsets: ["latin"],
@@ -24,11 +25,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={poppins.className}>
-        <Header />
-        <div className="flex">
-          <SideBar />
-          {children}
-        </div>
+        <ReduxProvider>
+          <Header />
+          <div className="flex">
+            <SideBar />
+            {children}
+          </div>
+        </ReduxProvider>
       </body>
     </html>
   );
