d647b7e534604e1897892234abf0dec52f67919f
alhassan50 <abdullahialhassanbaaba@gmail.com>
2024-07-02 09:30:05 +0000

    Create contact info data

---
+++ app/data/contactInfo.ts
@@ -0,0 +1,19 @@
+type ContactInfo = {
+    title: string;
+    description: string;
+}
+  
+export const contactInfo: ContactInfo[] = [
+    {
+      title: "Customer Support",
+      description: "Our support team is available around the clock to address any concerns or queries you may have."
+    },
+    {
+      title: "Feedback and Suggestions",
+      description: "We value your feedback and are continuously working to improve Snappy. Your input is crucial in shaping the future of Snappy."
+    },
+    {
+      title: "Media Inquiries",
+      description: "For media-related questions or press inquiries, please contact us at media@snappyapp.com."
+    }
+];
