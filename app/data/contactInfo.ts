444b4179ee3c977a0d5347ce75f8fa4586b10189
alhassan50 <abdullahialhassanbaaba@gmail.com>
2024-07-02 09:34:00 +0000

    Update contact info

--- app/data/contactInfo.ts
+++ app/data/contactInfo.ts
@@ -10,10 +10,6 @@ export const contactInfo: ContactInfo[] = [
     },
     {
       title: "Feedback and Suggestions",
-      description: "We value your feedback and are continuously working to improve Snappy. Your input is crucial in shaping the future of Snappy."
-    },
-    {
-      title: "Media Inquiries",
-      description: "For media-related questions or press inquiries, please contact us at media@snappyapp.com."
+      description: "We value your feedback and are continuously working to improve KAM. Your input is crucial in shaping our future and yours."
     }
 ];
