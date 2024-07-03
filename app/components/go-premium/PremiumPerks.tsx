3f0eb3f06358c27603650a4ba3ac9d945664939d
alhassan50 <abdullahialhassanbaaba@gmail.com>
2024-07-03 16:45:08 +0000

    Create sub section for premium advantages

---
+++ app/components/go-premium/PremiumPerks.tsx
@@ -0,0 +1,34 @@
+import { premiumAdvantages } from '@/app/data/premiumAdvantages'
+
+function PremiumPerks() {
+  return (
+    <ul className='grid gap-5 lg:gap-10 sm:grid-cols-2 lg:grid-cols-3 mt-20 md:max-w-[95%] mx-auto text-left'>
+        {premiumAdvantages.map(advantage => (
+          <li
+            key={advantage.title}
+          >
+            <figure className='w-full rounded-[4px] h-[200px] bg-[#0000008f]'>
+              {/* <Image
+                alt={advantage.title}
+                width={100}
+                height={100}
+                src={'/assets/images/email.svg'}
+              /> */}
+            </figure>
+            
+            <div className='mt-3'>
+              <h3>
+                {advantage.title}
+              </h3>
+
+              <p className='mt-1 text-sm'>
+                {advantage.description}
+              </p>
+            </div>
+          </li>
+        ))}
+      </ul>
+  )
+}
+
+export default PremiumPerks
