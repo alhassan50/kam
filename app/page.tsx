e0b3dc47dc55bbef0856e73327cec4848cab4c41
alhassan50 <abdullahialhassanbaaba@gmail.com>
2024-07-07 09:46:30 +0000

    Add testimonial section to hompage

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { Metadata } from "next";
 import Arrow from "./components/shared/Arrow";
 import { features } from "./data/features";
 import Link from "next/link";
+import { testimonials } from "./data/testimonials";
 
 export const metadata: Metadata = {
   title: 'Home | KAM',
@@ -74,11 +75,45 @@ export default function Home() {
     </section>
   )
 
+  const Testimonial = () => (
+    <section className='text-center pt-10 md:w-[90%] mx-auto grid gap-3'>
+      <h1 className="mx- text-left sm:text-center">
+        <span className="text-black">KAM</span> Impacting Lives
+      </h1>
+
+      <ul className="grid gap-5 sm:grid-cols-2 mt-5">
+        {testimonials.map(testimonial => (
+          <li key={testimonial.feedback}>
+            <div className="p-5 h-full flex flex-col justify-between border rounded border-[var(--bg-card)] hover:bg-[var(--hover-card)]">
+              <p className="text-left text-sm">
+                <svg className="inline-block h-4 w-6 rotate-180" height="48" viewBox="0 0 48 48" width="48" xmlns="http://www.w3.org/2000/svg"><path d="M12 34h6l4-8v-12h-12v12h6zm16 0h6l4-8v-12h-12v12h6z"/><path d="M0 0h48v48h-48z" fill="none"/></svg>
+                {testimonial.feedback}
+              </p>
+              <div className="flex gap-3 mt-5">
+                <figure className="w-14 h-14 bg-black rounded-[50%]"></figure>
+                <div>
+                  <h3 className="text-left">
+                    {testimonial.name}
+                  </h3>
+
+                  <p className="text-left text-xs">
+                    {testimonial.college}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </section>
+  )
+
   return (
     <main>
       <div>
         <Hero />
         <AboutKam />
+        <Testimonial />
       </div>
     </main>
   );
