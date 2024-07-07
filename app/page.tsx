137460c0be8dd63003ad8062f9ea3036e1bd2d3d
alhassan50 <abdullahialhassanbaaba@gmail.com>
2024-07-07 08:52:25 +0000

    Add hero section to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,40 @@
 import { Metadata } from "next";
+import Arrow from "./components/shared/Arrow";
 
 export const metadata: Metadata = {
   title: 'Home | KAM',
 };
 
 export default function Home() {
+  const Hero = () => (
+    <section className='text-center pt-10 grid gap-3'>
+      <h1 className="mx-auto">
+        <span className="text-black">Empower</span> Your Learning Journey with <br /> the <span className="text-black">AI-Enhanced <span title="Knowledge Enhancement Module" className="cursor-pointer">KAM</span></span>
+      </h1>
+
+      <p className='mt-2'>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo aliquid illo labore libero dolorem, modi <br /> accusantium quos impedit asperiores ullam.
+      </p>
+
+      <button
+          type="button"
+          className="btn-primary flex justify-center items-center gap-2 mx-auto mt-5 px-10"
+      >
+          Go Premium
+          <Arrow width={32} />
+      </button>
+
+      <figure className="relative rounded h-[500px] mt-10 overflow-hidden">
+        <div className="bg-[#0000008f] rounded-lg h-full w-full max-w-[1000px] mx-auto"></div>
+      </figure>
+    </section>
+  )
+  
   return (
-    <main>home</main>
+    <main>
+      <div>
+        <Hero />
+      </div>
+    </main>
   );
 }
