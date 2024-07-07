2c5865f1904ccc3126f31b1699d9c0247f5dade7
alhassan50 <abdullahialhassanbaaba@gmail.com>
2024-07-07 10:44:46 +0000

    Add future upgrades data

---
+++ app/data/upgrades.ts
@@ -0,0 +1,15 @@
+export const upgrades= [
+    {
+      title: "Mobile App",
+      description: "Stay connected and continue learning wherever you are with our upcoming mobile app, which gives you access to all your course materials and features on the go."
+    },
+    {
+      title: "Gamification Features",
+      description: "Our new gamification features will make learning fun and motivating. Earn points, badges, and rewards as you complete assessments and achieve your learning goals."
+    },
+    {
+      title: "Customizable Learning Paths",
+      description: "Our customizable learning paths will allow you to create personalized plans that suit your goals and preferences. Choose the subjects, modules, and pace that work best for you."
+    }
+];
+  
