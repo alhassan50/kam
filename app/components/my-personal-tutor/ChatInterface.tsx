62c6b2204a9f847cf20c0540d2049937fdb783e1
alhassan50 <abdullahialhassanbaaba@gmail.com>
2024-07-11 13:46:10 +0000

    Create chat interface

---
+++ app/components/my-personal-tutor/ChatInterface.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+function ChatInterface() {
+  return (
+    <div>
+        <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores atque neque id nemo fugiat odio, nobis error repellat molestiae odit soluta voluptatem iste nam, minima labore, asperiores sapiente! Natus, hic!
+            Aspernatur nam voluptate voluptatum eaque recusandae hic accusamus, velit, voluptas repellat doloremque enim? Suscipit alias similique rem natus corporis, mollitia maiores sint. Recusandae sunt animi incidunt nihil quaerat deserunt enim.
+            Molestias repellendus repellat ducimus pariatur illo molestiae ratione libero delectus, fugiat laboriosam veritatis laudantium voluptate ipsam, incidunt maxime neque sapiente sit maiores? Optio eum minima aut sapiente atque eligendi nobis!
+            Asperiores magni modi sapiente eligendi, temporibus provident sequi reprehenderit, ipsam harum dolor fugiat quos consectetur expedita nostrum vitae repellat? Vero ad itaque nemo quod est recusandae totam ut esse nostrum.
+            Natus cupiditate, pariatur nam perferendis omnis nesciunt maxime exercitationem, praesentium amet tenetur ipsa explicabo accusamus labore rerum tempora optio ipsum dolorem harum quod qui. Asperiores aliquid minus harum nihil debitis.
+            Ratione, provident vel ducimus nulla explicabo ad saepe deleniti? Facilis odit quas dolorum quam ab eos aperiam quo sit sapiente, dicta perferendis voluptatibus? Facere quod vitae quibusdam cupiditate provident omnis.
+            Iusto, nam! Molestiae ratione hic alias accusantium, maxime voluptatum quidem. Sunt rem commodi quos, voluptatem natus deleniti incidunt assumenda consequuntur quae voluptatibus maxime saepe numquam eveniet libero? Illum, voluptas aliquid?
+            Quas odio, est dolore vel dicta aut magnam culpa velit voluptatibus iusto voluptate tempore inventore facere, in, ipsam id beatae laudantium! Laboriosam id atque veniam et architecto officiis placeat quod.
+            Ducimus animi quae nam deleniti ut repellat hic molestiae laborum maiores officiis! Assumenda magni tempora incidunt? Voluptatem fugit voluptatibus quo sit dolorem ratione tempore ullam voluptates, explicabo quasi dolorum nihil.
+            Dolor ea rem, obcaecati alias accusantium saepe cumque, expedita facere aspernatur tenetur cum mollitia quae. Debitis corrupti at a nihil. Sed vero incidunt iusto laborum, obcaecati corporis vitae expedita deleniti?
+            Fugit illo maiores, id perspiciatis sint praesentium quo hic veritatis, nobis consequuntur accusantium fuga natus asperiores dolorum. Error harum quae excepturi, earum beatae autem, nostrum at ipsa, cum voluptatem nulla?
+            Nisi consectetur similique eaque soluta ea hic quas? Perspiciatis quos accusantium cupiditate saepe asperiores quis possimus animi obcaecati? Distinctio fugiat cupiditate, culpa iste nam molestiae cum accusamus aspernatur delectus iusto?
+            Labore atque in neque delectus voluptatibus itaque blanditiis sit alias, culpa dolorum cumque dignissimos reiciendis optio eos, voluptate rerum vero, illum deleniti eius esse beatae? Amet molestiae porro veniam quis!
+            Placeat maiores veniam quisquam unde, tempore vel mollitia temporibus enim, asperiores ducimus incidunt voluptatem assumenda iure labore! Neque sequi laboriosam voluptate quo et. Ut tempora quidem nam animi nulla illo.
+            Veniam est minima accusamus suscipit, perspiciatis exercitationem accusantium quasi architecto consequuntur aspernatur, a excepturi praesentium deleniti natus at perferendis eveniet? Sed placeat molestiae ut iste harum odio animi, qui soluta.
+            Est accusamus dolorem nobis eos nostrum laudantium tenetur non asperiores, nihil unde sit beatae voluptas officiis! Modi exercitationem a nemo tempora, architecto reprehenderit! Sunt aperiam soluta dicta velit, aspernatur officia!
+            Laboriosam adipisci accusantium soluta ex? Omnis, molestiae modi nostrum ullam optio nulla consectetur vel doloremque incidunt eum praesentium quisquam. Ex fuga vero praesentium blanditiis iusto alias, nulla velit ullam dolorum.
+            Id quia minus quis debitis natus, facere, nihil nam praesentium fugiat maiores aut odit? Impedit veritatis, architecto odio nesciunt corrupti deserunt dolorem temporibus iusto, soluta omnis in, distinctio ratione blanditiis?
+            Adipisci tenetur veritatis reprehenderit accusantium saepe illo modi facere quod similique cum dicta dolorum veniam rem excepturi odio earum eveniet, ab obcaecati exercitationem et voluptatum optio dolore ea! Veniam, perferendis!
+            Iste, repudiandae tempora facere ipsam quaerat earum illo necessitatibus facilis expedita officiis ratione quisquam laborum dolores veritatis quis autem rerum alias. Illo odio sapiente, obcaecati beatae velit quibusdam quidem est..
+        </p>
+    </div>
+  )
+}
+
+export default ChatInterface
