6e583145cfeb568d053b3c2731fdbed4e8871f00
alhassan50 <abdullahialhassanbaaba@gmail.com>
2024-07-15 10:33:40 +0000

    Remove Link wrapper from CloseModal component
    - Removed the Link component wrapping the button.
    - The CloseModal component now directly renders the button without navigation.

--- app/components/my-personal-tutor/CloseModal.tsx
+++ app/components/my-personal-tutor/CloseModal.tsx
@@ -2,15 +2,13 @@ import Link from 'next/link'
 
 function CloseModal({closeDialgue}: {closeDialgue: () => void}) {
   return (
-    <Link href='/my-personal-tutor'>
-        <button
-            type="button"
-            onClick={() => closeDialgue()} 
-            className="bg-primary h-6 w-6 flex justify-center items-center rounded-full text-secondary"
-        >
-            x
-        </button>
-    </Link>
+      <button
+          type="button"
+          onClick={() => closeDialgue()} 
+          className="bg-primary h-6 w-6 flex justify-center items-center rounded-full text-secondary"
+      >
+          x
+      </button>
   )
 }
 
