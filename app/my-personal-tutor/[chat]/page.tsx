c50b0c739df1e47498c606288d2e60e55446a16b
alhassan50 <abdullahialhassanbaaba@gmail.com>
2024-07-15 10:41:16 +0000

    Update Chat component to include ChatInterface
    - Imported and used ChatInterface component in the Chat page.
    - Removed previous placeholder content and updated layout to include only ChatInterface.

--- app/my-personal-tutor/[chat]/page.tsx
+++ app/my-personal-tutor/[chat]/page.tsx
@@ -1,6 +1,14 @@
+import AddNewChatButton from "@/app/components/my-personal-tutor/AddNewChatButton"
+import ChatInterface from "@/app/components/my-personal-tutor/ChatInterface"
+import ChatNav from "@/app/components/my-personal-tutor/ChatNav"
+import NewChat from "@/app/components/my-personal-tutor/NewChat"
+import TextArea from "@/app/components/my-personal-tutor/TextArea"
+
 function Chat() {
   return (
-    <div>Chat</div>
+    <div className="p-0">
+      <ChatInterface />
+    </div>
   )
 }
 
