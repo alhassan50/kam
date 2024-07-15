e4c00fa685133d2c704963057d34f45e44af01ed
alhassan50 <abdullahialhassanbaaba@gmail.com>
2024-07-15 10:35:18 +0000

    Add DefaultChatInterface component with welcome message
    - Created DefaultChatInterface component.
    - Added a welcome message to greet users with a personal tutor introduction.

---
+++ app/components/my-personal-tutor/DefaultChatInterface.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function DefaultChatInterface() {
+  return (
+    <div>
+        <h2 className='text-center leading-normal'>
+            Hello 👋🏽, I am your Personal Tutor! <br /> How can I help you today?
+        </h2>
+    </div>
+  )
+}
+
+
+export default DefaultChatInterface
