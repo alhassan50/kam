d3ec598e65c111ad97e96530ae226bdce91c7534
alhassan50 <abdullahialhassanbaaba@gmail.com>
2024-07-18 15:20:30 +0000

    Add link and hover effect in Hero component
    - Replaced button with Link component for navigation.
    - Introduced hover effect on arrow icon within the link for visual feedback.
    - Ensured smooth transition with group-hover utility and transition properties.

--- app/components/home/Hero.tsx
+++ app/components/home/Hero.tsx
@@ -1,4 +1,5 @@
 import Arrow from "../shared/Arrow"
+import Link from "next/link"
 
 const Hero = () => (
     <section className='text-center grid gap-3'>
@@ -10,13 +11,15 @@ const Hero = () => (
         Great learning comes with great tools. KAM is the best tool for us. Yep! Its that simple! 😃
       </p>
 
-      <button
-          type="button"
-          className="btn-primary flex justify-center items-center gap-2 mx-auto mt-5 px-10"
+      <Link
+          href='?login=y'
+          className="btn-primary group flex justify-center items-center gap-2 mx-auto mt-5 px-10"
       >
           Get Started
-          <Arrow width={32} />
-      </button>
+          <div className="gro group-hover:translate-x-1 transition-all duration-150">
+            <Arrow width={32} />
+          </div>
+      </Link>
 
       <figure className="relative rounded h-[500px] mt-10 overflow-hidden">
         <div className="bg-[#0000008f] rounded-lg h-full w-full max-w-[1000px] mx-auto"></div>
