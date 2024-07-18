752c94d933fbc4b09611da905fc7c28c59b64f76
alhassan50 <abdullahialhassanbaaba@gmail.com>
2024-07-18 14:40:51 +0000

    Enhance TitleBar to handle dynamic routes and improve UI
    - Updated TitleBar component:
      - Added logic to handle dynamic routes specifically for the /my-tutor path.
      - Introduced a nestedActiveLink variable to capture dynamic parts of the pathname.
      - Implemented formatNestedLink function to properly format dynamic route parts.
      - Set a title attribute for better accessibility and user experience.
      - Added styles to h3 element to handle text overflow with ellipsis.

--- app/components/shared/TitleBar.tsx
+++ app/components/shared/TitleBar.tsx
@@ -1,19 +1,37 @@
 'use client'
-
-import { usePathname } from 'next/navigation'
-import { navLinks } from '@/app/data/navLinks'
+import { usePathname } from 'next/navigation';
+import { navLinks } from '@/app/data/navLinks';
 
 function TitleBar() {
-  const pathname = usePathname()
-  const activeNavLink = navLinks.find(link => link.href === pathname)
+  const pathname = usePathname();
+  const activeNavLink = navLinks.find(link => link.href === pathname);
+
+  let nestedActiveLink = null;
+
+  if (!activeNavLink) {
+    const myTutorRegex = /^\/my-tutor\/.*/;
+    // Special case for /my-tutor dynamic routes
+    if (pathname.match(myTutorRegex)) {
+      // Extract the dynamic part from the pathname
+      const parts = pathname.split('/');
+      nestedActiveLink = parts[2]; // Adjust index based on your routing structure
+    }
+  }
+
+  // Function to format the nestedActiveLink
+  const formatNestedLink = (link: string) => {
+    if (!link) return 'Not Found';
+    // Convert to lowercase and replace hyphens with spaces
+    return decodeURIComponent(link.charAt(0).toUpperCase() + link.slice(1).replace(/-/g, ' '))
+  };
 
   return (
-    <div className='hidden md:block'>
-        <h3 className='font-medium capitalize text-sm'>
-          {activeNavLink ? activeNavLink.title : 'Not Found'}
+    <div className='hidden md:block' title={activeNavLink?.title || formatNestedLink(nestedActiveLink || '')}>
+        <h3 className='font-medium capitalize text-sm  max-w-[220px] whitespace-nowrap overflow-hidden overflow-ellipsis mx-auto'>
+          {activeNavLink ? activeNavLink.title : nestedActiveLink ? formatNestedLink(nestedActiveLink) : 'Not Found'}
         </h3>
     </div>
-  )
+  );
 }
 
-export default TitleBar
+export default TitleBar;
