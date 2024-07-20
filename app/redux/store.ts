aeaa1bf73bae774f26e1bc4e576dab45accdad92
alhassan50 <abdullahialhassanbaaba@gmail.com>
2024-07-20 22:22:56 +0000

    Add authReducer to Redux store configuration
    - Updated Redux store configuration to include `authReducer` for managing authentication state.

--- app/redux/store.ts
+++ app/redux/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import sideBarReducer from "./slices/sideBarSlice";
+import authReducer from './slices/authSlice'
 
 export const store = configureStore({
     reducer: {
-        sideBarReducer
+        sideBarReducer,
+        authReducer
     }
 })
