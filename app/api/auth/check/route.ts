77cc71029d8d597ffcdcfadb29d44e8ce7b2753a
alhassan50 <abdullahialhassanbaaba@gmail.com>
2024-07-22 15:23:33 +0000

    Create API route to check for auth status

---
+++ app/api/auth/check/route.ts
@@ -0,0 +1,13 @@
+// /app/api/auth/check/route.ts
+import { NextResponse } from 'next/server';
+import { cookies } from 'next/headers';
+
+export async function GET() {
+    const cookieStore = cookies();
+    const sessionCookie = cookieStore.get('session');
+    console.log("sessionCookie:::::::::::", sessionCookie);
+    
+    const isLoggedIn = !!sessionCookie;
+
+    return NextResponse.json({ isLoggedIn });
+}
