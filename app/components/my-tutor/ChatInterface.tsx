0a9c5477915ebbd06cdd36df261adc4b26e0345b
alhassan50 <abdullahialhassanbaaba@gmail.com>
2024-07-26 14:34:13 +0000

    Refactor ChatInterface component to fetch and display real-time chat data from Firebase, add loading and error states

--- app/components/my-tutor/ChatInterface.tsx
+++ app/components/my-tutor/ChatInterface.tsx
@@ -1,32 +1,132 @@
-import React from 'react'
+'use client'
 
-function ChatInterface() {
-  return (
-    <div>
-        <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores atque neque id nemo fugiat odio, nobis error repellat molestiae odit soluta voluptatem iste nam, minima labore, asperiores sapiente! Natus, hic!
-            Aspernatur nam voluptate voluptatum eaque recusandae hic accusamus, velit, voluptas repellat doloremque enim? Suscipit alias similique rem natus corporis, mollitia maiores sint. Recusandae sunt animi incidunt nihil quaerat deserunt enim.
-            Molestias repellendus repellat ducimus pariatur illo molestiae ratione libero delectus, fugiat laboriosam veritatis laudantium voluptate ipsam, incidunt maxime neque sapiente sit maiores? Optio eum minima aut sapiente atque eligendi nobis!
-            Asperiores magni modi sapiente eligendi, temporibus provident sequi reprehenderit, ipsam harum dolor fugiat quos consectetur expedita nostrum vitae repellat? Vero ad itaque nemo quod est recusandae totam ut esse nostrum.
-            Natus cupiditate, pariatur nam perferendis omnis nesciunt maxime exercitationem, praesentium amet tenetur ipsa explicabo accusamus labore rerum tempora optio ipsum dolorem harum quod qui. Asperiores aliquid minus harum nihil debitis.
-            Ratione, provident vel ducimus nulla explicabo ad saepe deleniti? Facilis odit quas dolorum quam ab eos aperiam quo sit sapiente, dicta perferendis voluptatibus? Facere quod vitae quibusdam cupiditate provident omnis.
-            Iusto, nam! Molestiae ratione hic alias accusantium, maxime voluptatum quidem. Sunt rem commodi quos, voluptatem natus deleniti incidunt assumenda consequuntur quae voluptatibus maxime saepe numquam eveniet libero? Illum, voluptas aliquid?
-            Quas odio, est dolore vel dicta aut magnam culpa velit voluptatibus iusto voluptate tempore inventore facere, in, ipsam id beatae laudantium! Laboriosam id atque veniam et architecto officiis placeat quod.
-            Ducimus animi quae nam deleniti ut repellat hic molestiae laborum maiores officiis! Assumenda magni tempora incidunt? Voluptatem fugit voluptatibus quo sit dolorem ratione tempore ullam voluptates, explicabo quasi dolorum nihil.
-            Dolor ea rem, obcaecati alias accusantium saepe cumque, expedita facere aspernatur tenetur cum mollitia quae. Debitis corrupti at a nihil. Sed vero incidunt iusto laborum, obcaecati corporis vitae expedita deleniti?
-            Fugit illo maiores, id perspiciatis sint praesentium quo hic veritatis, nobis consequuntur accusantium fuga natus asperiores dolorum. Error harum quae excepturi, earum beatae autem, nostrum at ipsa, cum voluptatem nulla?
-            Nisi consectetur similique eaque soluta ea hic quas? Perspiciatis quos accusantium cupiditate saepe asperiores quis possimus animi obcaecati? Distinctio fugiat cupiditate, culpa iste nam molestiae cum accusamus aspernatur delectus iusto?
-            Labore atque in neque delectus voluptatibus itaque blanditiis sit alias, culpa dolorum cumque dignissimos reiciendis optio eos, voluptate rerum vero, illum deleniti eius esse beatae? Amet molestiae porro veniam quis!
-            Placeat maiores veniam quisquam unde, tempore vel mollitia temporibus enim, asperiores ducimus incidunt voluptatem assumenda iure labore! Neque sequi laboriosam voluptate quo et. Ut tempora quidem nam animi nulla illo.
-            Veniam est minima accusamus suscipit, perspiciatis exercitationem accusantium quasi architecto consequuntur aspernatur, a excepturi praesentium deleniti natus at perferendis eveniet? Sed placeat molestiae ut iste harum odio animi, qui soluta.
-            Est accusamus dolorem nobis eos nostrum laudantium tenetur non asperiores, nihil unde sit beatae voluptas officiis! Modi exercitationem a nemo tempora, architecto reprehenderit! Sunt aperiam soluta dicta velit, aspernatur officia!
-            Laboriosam adipisci accusantium soluta ex? Omnis, molestiae modi nostrum ullam optio nulla consectetur vel doloremque incidunt eum praesentium quisquam. Ex fuga vero praesentium blanditiis iusto alias, nulla velit ullam dolorum.
-            Id quia minus quis debitis natus, facere, nihil nam praesentium fugiat maiores aut odit? Impedit veritatis, architecto odio nesciunt corrupti deserunt dolorem temporibus iusto, soluta omnis in, distinctio ratione blanditiis?
-            Adipisci tenetur veritatis reprehenderit accusantium saepe illo modi facere quod similique cum dicta dolorum veniam rem excepturi odio earum eveniet, ab obcaecati exercitationem et voluptatum optio dolore ea! Veniam, perferendis!
-            Iste, repudiandae tempora facere ipsam quaerat earum illo necessitatibus facilis expedita officiis ratione quisquam laborum dolores veritatis quis autem rerum alias. Illo odio sapiente, obcaecati beatae velit quibusdam quidem est..
-        </p>
-    </div>
-  )
+import React, { useEffect, useRef, useState } from 'react';
+import { getFirestore, doc, onSnapshot } from 'firebase/firestore';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import Spinner from '../shared/Spinner';
+import TimeAgo from 'react-time-ago';
+import javascriptTimeAgo from 'javascript-time-ago';
+import en from 'javascript-time-ago/locale/en';
+
+javascriptTimeAgo.addDefaultLocale(en);
+
+interface Chat {
+  id: string;
+  timeCreated: string;
+  role: string;
+  content: string;
 }
 
-export default ChatInterface
+function ChatInterface({ slideId }: { slideId: string }) {
+  const [chat, setChat] = useState<Chat[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<boolean | null>(null);
+
+  // Ref for the chat container
+  const chatEndRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const db = getFirestore();
+    const auth = getAuth();
+
+    // Function to handle authenticated user's data fetching
+    const handleAuthChange = (user: any) => {
+      if (user) {
+        const userId = user.uid;
+        const userDocRef = doc(db, `users/${userId}`);
+
+        // Set up a real-time listener for the user's chat list
+        const unsubscribeChat = onSnapshot(userDocRef, (userDoc) => {
+          if (userDoc.exists()) {
+            const chatList = userDoc.data()?.chatList || [];
+            const slideData = chatList.find((item: any) => item.slideId === slideId);
+            if (slideData) {
+              setChat(slideData.chat || []);
+            } else {
+              setChat([]);
+            }
+          } else {
+            setChat([]);
+          }
+          setLoading(false);
+        }, (error) => {
+          setError(true);
+          setLoading(false);
+        });
+
+        // Clean up the Firestore listener on component unmount
+        return () => {
+          unsubscribeChat();
+        };
+      } else {
+        setLoading(false);
+      }
+    };
+
+    // Set up listener for auth changes
+    const unsubscribeAuth = onAuthStateChanged(auth, handleAuthChange);
+
+    // Clean up auth listener on component unmount
+    return () => {
+      unsubscribeAuth();
+    };
+  }, [slideId]);
+
+  // Scroll to bottom whenever chat updates
+  useEffect(() => {
+    if (chatEndRef.current) {
+      chatEndRef.current.scrollIntoView({ behavior: 'smooth', block: 'end' });
+    }
+  }, [chat]);
+
+  if (loading) {
+    return (
+      <div className='h-full flex items-center justify-center'>
+        <Spinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className='h-full flex items-center justify-center'>
+        <h2 className='text-center leading-normal'>
+          Error loading chat data.
+        </h2>
+      </div>
+    );
+  }
+
+  return (
+    <div className='h-full p-4 sm:w-[90%] flex flex-col gap-4 md:w-[80%] mx-auto text-sm'>
+      {chat.length > 0 ? (
+        <>
+          {chat.map((chatItem) => (
+            <div key={chatItem.id} className={`grid grid-flow-col gap-3 max-w-[60%] ${chatItem.role === 'user' ? 'ml-auto' : 'mr-auto' }`}>
+              <div className={`flex flex-col items-end gap-2`}>
+                <div className='bg-[var(--chat-bubble)] px-4 py-2 rounded-lg'>
+                  <pre className='text-wrap break-all font-extralight'>{chatItem.content}</pre>
+                </div>
+                <small className='inline-block font-extralight'><TimeAgo date={new Date(chatItem.timeCreated)} /></small>
+                {/* <small className='inline-block font-extralight'>{new Date(chatItem.timeCreated).toLocaleString()}</small> */}
+              </div>
+              <figure className='w-6 h-6 rounded-full p-1 bg-tertiary'>            
+                  <svg viewBox="0 0 256 256" className='w-full' xmlns="http://www.w3.org/2000/svg"><rect fill="none" className='w-full h-full'/><circle cx="128" cy="96" fill="none" r="64" stroke="#000" strokeMiterlimit="10" strokeWidth="16"/><path d="M31,216a112,112,0,0,1,194,0" fill="none" stroke="#000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16"/></svg>
+              </figure>
+            </div>
+          ))}
+          {/* This div will be used to scroll to the bottom */}
+          <div ref={chatEndRef} />
+        </>
+      ) : (
+        <div className='h-full flex items-start  justify-center'>
+          <h2 className='text-center leading-normal'>
+            Hello 👋🏽, I am your Personal Tutor! <br /> How can I help you today?
+          </h2>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default ChatInterface;
