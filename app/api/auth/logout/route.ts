d3838534c0b1330bea94f4a7461f81d814391a16
alhassan50 <abdullahialhassanbaaba@gmail.com>
2024-07-27 18:06:20 +0000

    Destroy user cookie on logout

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -1,14 +1,23 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function POST(request: NextRequest) {
-    console.log('heyyy');
-    
+export async function POST(request: NextRequest) {    
     try {
         const response = NextResponse.json({ message: 'Logged out successfully' });
 
-        // Set the session cookie to an empty value and expire it
+        // Clear the session cookie
         response.cookies.set({
             name: 'session',
+            value: '', // Clear the cookie value
+            maxAge: 0, // Expire the cookie immediately
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production', // Only send cookie over HTTPS
+            path: '/',
+            sameSite: 'strict', // or 'lax' depending on your requirements
+        });
+
+        // Clear the user cookie
+        response.cookies.set({
+            name: 'user',
             value: '', // Clear the cookie value
             maxAge: 0, // Expire the cookie immediately
             httpOnly: true,
