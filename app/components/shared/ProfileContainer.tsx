5e558178a8a538d630ac49bbd99bf70c59d59b58
alhassan50 <abdullahialhassanbaaba@gmail.com>
2024-07-27 17:59:36 +0000

    Enhance ProfileContainer with user info and loading state
    - Added user state to fetch and display the current user's display name and email.
    - Integrated Firebase Authentication to handle user state changes.
    - Incorporated a loading spinner to indicate when user data is being fetched.
    - Updated profile menu to include user information and conditionally render a loading spinner.
    - Utilized Redux selector to manage updates related to account info changes.
    - Improved JSX structure and added conditional rendering for user info and loading states.

--- app/components/shared/ProfileContainer.tsx
+++ app/components/shared/ProfileContainer.tsx
@@ -4,10 +4,17 @@ import { useState, useEffect, useRef } from 'react';
 import Profile from './Profile';
 import Link from 'next/link';
 import { menuItems } from '@/app/data/profileMenuItems';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { CircularProgress } from '@mui/material';
+import { useSelector } from 'react-redux';
+import { selectHasAccountInfoChanged } from '@/app/redux/slices/accountInfoSlice';
 
 function ProfileContainer() {
   const [isProfileMenuOpened, setIsProfileMenuOpened] = useState<boolean>(false);
+  const [user, setUser] = useState<{ displayName: string; email: string } | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
   const profileRef = useRef<HTMLDivElement>(null);
+  const hasAccountInfoChanged = useSelector(selectHasAccountInfoChanged);
 
   const toggleProfileMenu = () => {
     setIsProfileMenuOpened((prev) => !prev);
@@ -29,29 +36,56 @@ function ProfileContainer() {
     };
   }, [isProfileMenuOpened]);
 
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser({ displayName: user.displayName || '', email: user.email || '' });
+      } else {
+        setUser(null);
+      }
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, [hasAccountInfoChanged]);
+
   return (
     <div ref={profileRef} className='relative h-full flex justify-center items-center'>
       <Profile toggleProfileMenu={toggleProfileMenu} />
 
       {isProfileMenuOpened && (
         <div className='profile-menu absolute right-0 text-nowrap min-w-[200px] text-[12px] top-[110%] p-2 border border-[var(--bg-card)] bg-primary rounded grid shadow-lg'>
-          {menuItems.map((item, index) => (
-            <>
-                {index === menuItems.length-1 &&
-                    <hr className='my-1 border border-[var(--bg-card)]' />
-                }
-                <Link 
-                    key={index} 
-                    href={item.href} 
-                    className='p-2 hover:bg-[var(--hover-card)] rounded flex items-center gap-2'
-                    title={item.label}
-                    onClick={() => toggleProfileMenu()}
+          <div>
+            {
+              loading ? 
+              (<div className='flex justify-center items-center py-2'>
+                  <CircularProgress />
+                </div>)
+                : 
+              (<div>
+                  <div className="p-2 flex flex-col gap-1">
+                  <span className='whitespace-nowrap overflow-hidden overflow-ellipsis max-w-[200px]' >{user?.displayName}</span>
+                  <span className='whitespace-nowrap overflow-hidden overflow-ellipsis max-w-[200px]'>{user?.email}</span>
+                </div>
+                {user?.email &&  <hr className='my-1 border border-[var(--bg-card)]' />}
+                </div>)
+            }
+            {menuItems.map((item, index) => (
+              <div key={index}>
+                {index === menuItems.length - 1 && <hr className='my-1 border border-[var(--bg-card)]' />}
+                <Link
+                  href={item.href}
+                  className='p-2 hover:bg-[var(--hover-card)] rounded flex items-center gap-2'
+                  title={item.label}
+                  onClick={() => toggleProfileMenu()}
                 >
-                    <figure>{item.svg}</figure>
-                    <h3>{item.label}</h3>
+                  <figure>{item.svg}</figure>
+                  <h3>{item.label}</h3>
                 </Link>
-            </>
-          ))}
+              </div>
+            ))}
+          </div>
         </div>
       )}
     </div>
