033bf48caa15ace98ccb3a20bd2455dcc130172d
alhassan50 <abdullahialhassanbaaba@gmail.com>
2024-07-27 17:44:25 +0000

    Add accountInfoReducer to Redux store configuration
    - Imported `accountInfoReducer` from `./slices/accountInfoSlice`.
    - Updated the `configureStore` function to include `accountInfoReducer` in the list of reducers.

--- app/redux/store.ts
+++ app/redux/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import sideBarReducer from "./slices/sideBarSlice";
 import authReducer from './slices/authSlice'
+import accountInfoReducer from './slices/accountInfoSlice'
 
 export const store = configureStore({
     reducer: {
         sideBarReducer,
-        authReducer
+        authReducer,
+        accountInfoReducer
     }
 })
