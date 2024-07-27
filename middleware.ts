246c3ad154d74b1991142c23e26aafad7782024d
alhassan50 <abdullahialhassanbaaba@gmail.com>
2024-07-27 17:46:08 +0000

    Add middleware for session management and access control
    - Created middleware to check for a session cookie in incoming requests.
    - Redirect users to the login page with the original URL as a query parameter if the session cookie is absent.
    - Allowed access to routes `/my-tutor/:path*`, `/take-assessment/:path*`, and `/settings` only if the session cookie is present.
    - Configured the middleware matcher to apply the session check to the specified routes.

---
+++ middleware.ts
@@ -0,0 +1,15 @@
+import { NextResponse, NextRequest } from "next/server"
+
+export function middleware (request: NextRequest) {
+    const sessionCookie = request.cookies.get('session')
+    //console.log(sessionCookie);
+    const originalUrl = encodeURIComponent(request.url);
+    
+    if (!sessionCookie) return NextResponse.redirect(new URL(`/?login=y&redirect=${originalUrl}`, request.url));
+
+    return NextResponse.next()
+}
+
+export const config = {
+    matcher: ['/my-tutor/:path*', '/take-assessment/:path*', '/settings' ]
+}
